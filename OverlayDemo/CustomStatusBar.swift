cf91edf9d6ebbcd0c751dc6d15e764d4e123547b
Patrick Lin <patricksclin@gmail.com>
2017-09-26 18:58:02 +0800

    first commit

---
+++ OverlayDemo/CustomStatusBar.swift
@@ -0,0 +1,21 @@
+//
+//  CustomStatusBar.swift
+//  OverlayDemo
+//
+//  Created by Patrick Lin on 26/09/2017.
+//  Copyright © 2017 Soocii. All rights reserved.
+//
+
+import UIKit
+
+class CustomStatusBar: UIWindow {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
